74c0dafce10289dbcf62f83ca107222ee40dea78
Leonardo Henrique Paim <leohpaim@gmail.com>
2024-06-13 14:36:07 -0300

    feat: criado `controller` de clientes

---
+++ src/Presentation/NossaLoja.WebService/Controllers/ClienteController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NossaLoja.Cadastros.Application.Application;
+using NossaLoja.Cadastros.Application.ViewModels;
+
+namespace NossaLoja.WebService.Controllers;
+
+[Route("Api/Clientes")]
+[ApiController]
+public class ClienteController : ControllerBase
+{
+    private ClienteApplication ClienteApplication { get; set; }
+
+    public ClienteController()
+    {
+        ClienteApplication = new ClienteApplication();
+    }
+
+    [HttpGet]
+    [Route("GetAll")]
+    public IActionResult GetAll()
+    {
+        try
+        {
+            var clientes = ClienteApplication.GetAll();
+
+            return StatusCode((int)ClienteApplication.StatusCode, new { Clientes = clientes });
+        }
+        catch (Exception)
+        {
+            return BadRequest("Erro ao buscar os clientes.");
+        }
+        
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            var cliente = ClienteApplication.Get(id);
+
+            return StatusCode((int)ClienteApplication.StatusCode, cliente);
+        }
+        catch (Exception)
+        {
+            return BadRequest("Erro ao buscar o cliente");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Add(ClienteVM cliente)
+    {
+        try
+        {
+            ClienteApplication.Add(cliente);
+
+            return StatusCode((int)ClienteApplication.StatusCode, $"Add cliente {cliente.Nome}.");
+        }
+        catch (Exception)
+        {
+            return BadRequest("Erro ao adicionar o cliente");
+        }
+    }
+
+    [HttpPut]
+    public IActionResult Update(ClienteVM cliente)
+    {
+        try
+        {
+            ClienteApplication.Update(cliente);
+
+            return StatusCode((int)ClienteApplication.StatusCode, $"Atualizar cliente {cliente.Nome}.");
+        }
+        catch (Exception)
+        {
+            return BadRequest("Erro ao atualizar o cliente");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        try
+        {
+            ClienteApplication.Delete(id);
+
+            return StatusCode((int)ClienteApplication.StatusCode, $"Deleta cliente {id}.");
+        }
+        catch (Exception)
+        {
+            return BadRequest("Erro ao deletar o cliente");
+        }
+    }
+}
---
+++ src/Presentation/NossaLoja.WebService/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Usar Pascal Case
+        options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var container = NossaLoja.Cadastros.Infra.DependencyInjection.Services.Container.GetContainer();
+NossaLoja.Cadastros.Infra.DependencyInjection.Services.DependencyInjectionService.Inicializa(container);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
